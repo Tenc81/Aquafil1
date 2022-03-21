495c178c8d5b8768ca08075f33c2f6f4a8de30df
Luca Zampetti <lzampetti@gmail.com>
2022-03-21 17:12:05 +0100

    Fixing - css mobile

--- wp-content/themes/aquafil/templates/partials/shared/generic-text.php
+++ wp-content/themes/aquafil/templates/partials/shared/generic-text.php
@@ -8,6 +8,12 @@ $_args = wp_parse_args(
 if(!empty($_args['section'])) :
 ?>
 <div class="section-generic">
-	<div class="container"><?= $_args['section']["text"]; ?></div>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-sm-20 offset-sm-2 col-md-18 offset-md-3">
+        <?= $_args['section']["text"]; ?>
+      </div>
+    </div>
+  </div>
 </div>
 <?php endif; ?>
