5577682cd07a2255fe198cc93c7431b7cbe4f6b7
Luca Zampetti <lzampetti@gmail.com>
2022-07-20 12:37:31 +0200

    Modified: contacts-hero + privacy css

--- wp-content/themes/aquafil/templates/partials/contacts/contacts-hero.php
+++ wp-content/themes/aquafil/templates/partials/contacts/contacts-hero.php
@@ -26,7 +26,7 @@ if(!empty($_args['section'])) :
 						echo '
 							<a href="//www.google.it/maps/search/'.str_replace(' ', '+', trim($_args['section']['indirizzo_cnt_hero'])).'" target="_blank">
 								<img loading="lazy" src="//maps.googleapis.com/maps/api/staticmap?
-                style=feature%3Alandscape%7Celement%3Alabels%7Cvisibility%3Aoff%7C&amp;style=feature%3Atransit%7Celement%3Alabels%7Cvisibility%3Aoff%7C&amp;style=feature%3Apoi%7Celement%3Alabels%7Cvisibility%3Aoff%7C&amp;style=feature%3Awater%7Celement%3Alabels%7Cvisibility%3Aoff%7C&amp;style=feature%3Aroad%7Celement%3Alabels.icon%7Cvisibility%3Aoff%7C&amp;style=feature%3Aall%7Celement%3Aall%7Chue%3A0x00aaff%7Csaturation%3A-100%7Cgamma%3A2.15%7Clightness%3A12%7C&amp;style=feature%3Aroad%7Celement%3Alabels.text.fill%7Cvisibility%3Aon%7Clightness%3A24%7C&amp;style=feature%3Aroad%7Celement%3Ageometry%7Clightness%3A57%7C
+                style=feature%3Aadministrative%7Celement%3Alabels.text.fill%7Ccolor%3A0x444444%7C&style=feature%3Alandscape%7Celement%3Aall%7Ccolor%3A0xf2f2f2%7C&style=feature%3Apoi%7Celement%3Aall%7Cvisibility%3Aoff%7C&style=feature%3Aroad%7Celement%3Aall%7Csaturation%3A-100%7Clightness%3A45%7C&style=feature%3Aroad.highway%7Celement%3Aall%7Cvisibility%3Asimplified%7C&style=feature%3Aroad.arterial%7Celement%3Alabels.icon%7Cvisibility%3Aoff%7C&style=feature%3Atransit%7Celement%3Aall%7Cvisibility%3Aoff%7C&style=feature%3Awater%7Celement%3Aall%7Ccolor%3A0x46bcec%7Cvisibility%3Aon%7C
                 &style=feature:poi|visibility:off
                 &zoom=14&scale=1&size=800x600&maptype=roadmap
                 &markers=color:0xffffff00%7Csize:small%7C'.str_replace(' ', '+', trim($_args['section']['indirizzo_cnt_hero'])).'
