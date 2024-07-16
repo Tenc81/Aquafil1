55818cb63fc072f145887d670bdd89378c3bd6a0
Giampiero Calma <g.calma@websolute.it>
2024-07-16 14:52:04 +0200

    correzione componente accordion IR con filtri

--- wp-content/themes/aquafil/templates/partials/investors/accordion-filters.php
+++ wp-content/themes/aquafil/templates/partials/investors/accordion-filters.php
@@ -7,27 +7,7 @@ $_args = wp_parse_args(
   ));
 if(!empty($_args['filters']) || !empty($_args['oldfilters'])) :
 ?>
-<script type="text/javascript">
-  (function($) {
-    $(".history-filter").on("click", function() {
-      $("#financial-calendar .card--calendar").show();
-      $(".history-filter").removeClass("active");
-      $(this).addClass("active");
-      if($(this).attr("data-filter")!="all") {
-        $("#financial-calendar .card--calendar").not("."+$(this).attr("data-filter")).hide();
-      }
-    });
-    $("#switch_events").on("click", function() {
-      $("span", this).toggleClass("hidden");
-      $(".history-filter").removeClass("active");
-      $(".nav--horizontal-menu, .page--investors__section-title", '#financial-calendar').toggleClass("hidden");
-      $('.history-filter:first', '.nav--horizontal-menu').addClass("active");
-      $("#financial-calendar .card--calendar").removeAttr("style");
-      $("#financial-calendar .card--calendar").toggleClass("hidden");
-    });
-  })(jQuery);
-</script>
-<h2 class="page--investors__section-title"><?= __("Programmed events", "wstheme"); ?></h2>
+<h2 class="page--investors__section-title"><?= __("Upcoming events", "wstheme"); ?></h2>
 <h2 class="page--investors__section-title hidden"><?= __("Past events", "wstheme"); ?></h2>
 
 <div class="horizontal-menu">
@@ -72,7 +52,27 @@ if(!empty($_args['filters']) || !empty($_args['oldfilters'])) :
 
 <a href="javascript:void(0);" class="btn--more-md" id="switch_events">
   <span><?= __("Past events", "wstheme"); ?></span>
-  <span class="hidden"><?= __("Programmed events", "wstheme"); ?></span>
+  <span class="hidden"><?= __("Upcoming events", "wstheme"); ?></span>
   <svg><use xlink:href="#arrow-next"></use></svg>
 </a>
+<script type="text/javascript">
+  (function($) {
+    $(".history-filter").on("click", function() {
+      $("#financial-calendar .card--calendar").show();
+      $(".history-filter").removeClass("active");
+      $(this).addClass("active");
+      if($(this).attr("data-filter")!="all") {
+        $("#financial-calendar .card--calendar").not("."+$(this).attr("data-filter")).hide();
+      }
+    });
+    $("#switch_events").on("click", function() {
+      $("span", this).toggleClass("hidden");
+      $(".history-filter").removeClass("active");
+      $(".nav--horizontal-menu, .page--investors__section-title", '#financial-calendar').toggleClass("hidden");
+      $('.history-filter:first', '.nav--horizontal-menu').addClass("active");
+      $("#financial-calendar .card--calendar").removeAttr("style");
+      $("#financial-calendar .card--calendar").toggleClass("hidden");
+    });
+  })(jQuery);
+</script>
 <?php endif; ?>
