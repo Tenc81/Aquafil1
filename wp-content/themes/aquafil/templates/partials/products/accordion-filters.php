ff6a4e31b23cf1c26bab7b0c6349231737c32b12
Giacomo Gennari <g.gennari@websolute.it>
2025-05-29 17:29:22 +0200

    nuovo template prodotti

---
+++ wp-content/themes/aquafil/templates/partials/products/accordion-filters.php
@@ -0,0 +1,47 @@
+<?php
+$_args = wp_parse_args(
+  $args,
+  array(
+    'filters' => array(),
+    'oldfilters' => array()
+  ));
+if(!empty($_args['filters']) || !empty($_args['oldfilters'])) :
+?>
+
+<div class="horizontal-menu">
+  <div class="horizontal-menu__content">
+    <div class="horizontal-menu__title"><?= __("Filter by", "wstheme"); ?></div>
+    <?php if(!empty($_args['oldfilters'])) : ?>
+    <ul class="nav--horizontal-menu">
+      <?php $i=0;
+      foreach($_args['oldfilters'] as $filter) : 
+      ?>
+      <li class="nav__item">
+        <a href="javascript:void(0);" class="history-filter <?= $i==0 ? "active" : ''; ?>" data-filter="<?= $filter["value"];  ?>">
+          <span class="name"><?= $filter["label"]; ?></span>
+          <span class="count">(<?= $filter["count"]; ?>)</span>
+        </a>
+      </li>
+      <?php 
+      $i++; 
+      endforeach; 
+      ?>
+    </ul>
+    <?php endif; ?>
+  </div>
+</div>
+
+
+<script type="text/javascript">
+  (function($) {
+    $(".history-filter").on("click", function() {
+      $("#financial-calendar .card--calendar").show();
+      $(".history-filter").removeClass("active");
+      $(this).addClass("active");
+      if($(this).attr("data-filter")!="all") {
+        $("#financial-calendar .card--calendar").not("."+$(this).attr("data-filter")).hide();
+      }
+    });
+  })(jQuery);
+</script>
+<?php endif; ?>
---
+++ wp-content/themes/aquafil/templates/partials/products/sidebar.php
@@ -0,0 +1,75 @@
+             <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-sm-6 offset-sm-1 col-md-5 offset-md-2">
+                                <div class="product-list__sidebar">
+                                    <div class="sidebar--item material">
+                                        <button type="button" class="btn--title" toggle=".material"><span>Material</span>
+                                            <svg>
+                                                <use xlink:href="#caret-down"></use>
+                                            </svg>
+                                        </button>
+                                        <div class="sidebar--content">
+                                            <div class="list-filters">
+                                                <button class="btn btn--outline">PA6</button>
+                                                <button class="btn btn--outline">PA66</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="sidebar--item recycled-type">
+                                        <button type="button" class="btn--title" toggle=".recycled-type"><span>Recycled type</span>
+                                            <svg>
+                                                <use xlink:href="#caret-down"></use>
+                                            </svg>
+                                        </button>
+                                        <div class="sidebar--content">
+                                            <div class="list-filters">
+                                                <button class="btn btn--outline">Chemically Regenerated (PCR)</button>
+                                                <button class="btn btn--outline">Mechanically recycled (PIR)</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="sidebar--item brand">
+                                        <button type="button" class="btn--title" toggle=".brand"><span>Brand</span>
+                                            <svg>
+                                                <use xlink:href="#caret-down"></use>
+                                            </svg>
+                                        </button>
+                                        <div class="sidebar--content">
+                                            <div class="list-filters">
+                                                <button class="btn btn--outline">Aquamid R</button>
+                                                <button class="btn btn--outline">Aquamid</button>
+                                                <button class="btn btn--outline">ECONYL&reg; Regenerated Nylon</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="sidebar--item properties">
+                                        <button type="button" class="btn--title" toggle=".properties"><span>Properties</span>
+                                            <svg>
+                                                <use xlink:href="#caret-down"></use>
+                                            </svg>
+                                        </button>
+                                        <div class="sidebar--content">
+                                            <div class="based-polymer">
+                                                <div class="filters-title">Based Polymer</div>
+                                                <div class="list-filters">
+                                                    <button class="btn btn--outline">Raw polymer</button>
+                                                    <button class="btn btn--outline">Lubricated+Nucleated</button>
+                                                    <button class="btn btn--outline">Food contact</button>
+                                                </div>
+                                            </div>
+                                            <div class="engineered-polymers">
+                                                <div class="filters-title">Engineered polymers</div>
+                                                <div class="list-filters">
+                                                    <button class="btn btn--outline">Unfilled</button>
+                                                    <button class="btn btn--outline">Filled</button>
+                                                    <button class="btn btn--outline">Impact modified</button>
+                                                    <button class="btn btn--outline">Heat aging stabilised</button>
+                                                    <button class="btn btn--outline">Uv stabilised</button>
+                                                    <button class="btn btn--outline">Food contact</button>
+                                                    <button class="btn btn--outline">Colored</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
